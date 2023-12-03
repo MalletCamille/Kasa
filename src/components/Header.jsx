5f30e2d3a4406c40e9ffd6e200c90ce1ab50a56b
Camille Mallet <mallet.camille11@gmail.com>
2023-12-03 18:22:18 +0100

    feat : underligne at current page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,28 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import logokasa from '../assets/LOGO.svg'
 import '../style/Header.scss'
+import { useEffect, useState } from 'react';
 
 function Header() {
+    const location = useLocation();
+    const [currentPage, setCurrentPage] = useState('Accueil');
+
+    useEffect(() => {
+        const path = location.pathname;
+        if (path === '/') {
+            setCurrentPage('Accueil');
+        } 
+        else if (path === '/About') {
+            setCurrentPage('About')
+        }
+    }, [location.pathname]);
+
     return (
         <header className = "header">
                 <img className= "header__logo" src={logokasa} alt="logo Kasa" />
-                <nav className = "header__nav">
-                    <Link className='header__link' to="/">Accueil</Link>
-                    <Link className='header__link' to="/About">A Propos</Link>
+                <nav className="header__nav">
+                    <Link to="/" className={`header__link ${currentPage === 'Accueil' ? 'active' : ''}`}>Accueil</Link>
+                    <Link to="/About" className={`header__link ${currentPage === 'About' ? 'active' : ''}`}>A Propos</Link>
                 </nav>
         </header>
     )
