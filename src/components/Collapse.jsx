12cc708e4f62780f131ceb6f9034811c9b3c5bf3
Camille Mallet <mallet.camille11@gmail.com>
2023-12-04 18:33:57 +0100

    feat : collapse animation wip

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,14 +1,27 @@
 import '../style/Collapse.scss'
 import arrow from '../assets/arrow.png'
+import React, { Fragment, useState } from 'react';
 
+function Collapse({ collapsename, children }) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
+  function handleClickArrow() {
+    setIsCollapsed(!isCollapsed);
+  }
 
-function Collapse({ collapsename }) {
-    return (
-        <button className = "collapse" id="collapse">
-        {collapsename} <img className='collapse__arrow'src={arrow}></img> 
-        </button>
-    )
+  return (
+   <Fragment>
+      <button className="collapse__button" onClick={handleClickArrow}>
+        {collapsename} <img className={`collapse__arrow ${isCollapsed ? '--collapsed' : '--expanded'}`} src={arrow} alt="arrow" />
+      </button>
+
+      {isCollapsed ? null : (
+        <div className="collapse__details">
+          {children}
+        </div>
+      )}
+    </Fragment>
+  );
 }
 
-export default Collapse
+export default Collapse;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,7 @@ import Banner from '../../components/Banner.jsx'
 import imgbannerabout from '../../assets/Banner_About.png'
 import Collapse from '../../components/Collapse.jsx'
 import { Fragment } from 'react'
+import '../../style/Collapse.scss'
 
 function About() {
     return (
@@ -10,10 +11,16 @@ function About() {
            <Banner Tagline={false} image={imgbannerabout} />
         </div>
         <div className='collapse__container'>
-          <Collapse collapsename="Fiabilité" />
+          <Collapse collapsename="Fiabilité">
+            <span className='collapse__text'>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</span>  
+          </Collapse>  
+          {/* <div className="collapse__details">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</div>
           <Collapse collapsename="Respect" />
+          <div className="collapse__details">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</div>
           <Collapse collapsename="Service" />
+          <div className="collapse__details">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</div>
           <Collapse collapsename="Sécurité" />
+          <div className="collapse__details">La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes</div> */}
         </div>
      </Fragment> 
   )
