91df49782a44e5233f1f31a9c6f0f5e1abb715f8
Camille Mallet <mallet.camille11@gmail.com>
2023-12-06 11:43:31 +0100

    Feat : add rating

--- src/pages/Appartment/Appartment.jsx
+++ src/pages/Appartment/Appartment.jsx
@@ -6,6 +6,8 @@ import json from '../../../data/appartments.json'
 import '../../style/Appartment.scss'
 import '../../style/Tag.scss'
 import Collapse from '../../components/Collapse.jsx'
+import starfilled from '../../assets/star-active.png'
+import starempty from '../../assets/star-inactive.png'
 
 Appartment.propTypes = {
   cover: PropTypes.string.isRequired
@@ -16,23 +18,45 @@ Appartment.propTypes = {
   const { appartmentid } = useParams();
   const currentappartment = json.find(appartment => appartment.id === appartmentid);
 
+  const renderRatingStars = () => {
+    const rating = parseInt(currentappartment.rating);
+    const stars = [];
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img key={i} src={starfilled} alt="Étoile remplie" />);
+      } else {
+        stars.push(<img key={i} src={starempty} alt="Étoile vide" />);
+      }
+    }
+    return stars;
+  };
+
+
   return (
     <div className='appartment__container'>
       <Slideshow cover={currentappartment.cover} />
-      <div className='appartment__wrap'>
-        <div className='appartment__infos'>
-          <h1 className='appartment__title'>{currentappartment.title}</h1>
-          <p className='appartment__location'>{currentappartment.location}</p>
-        </div>  
+      <div className='wrapper'>
+        <div className='appartmenttags__wrap'>
+          <div className='appartment__infos'>
+            <h1 className='appartment__title'>{currentappartment.title}</h1>
+            <p className='appartment__location'>{currentappartment.location}</p>
+          </div>
+          <div className='tags__container'>
+            {currentappartment.tags.map((tag, index) => (
+            <Tag key={index} description={tag} className='appartment__tag' />))}
+          </div>
+        </div>      
+        <div className='hostrating__wrap'>  
           <div className='host'>
             <span className='host__name'>{currentappartment.host.name}</span>
             <img className='host__pix' src={currentappartment.host.picture}></img>  
           </div>
-      </div>          
-      <div className='tags__container'>
-        {currentappartment.tags.map((tag, index) => (
-        <Tag key={index} description={tag} className='appartment__tag' />))}
-      </div>
+            <div className='rating__container'>
+              {renderRatingStars()}    
+            </div>
+        </div>
+      </div>        
       <div className='collapse__content'>
         <Collapse className='collapse' collapsename="Description">
           <span className='collapse__text'>{currentappartment.description}</span> 
@@ -40,7 +64,7 @@ Appartment.propTypes = {
         <Collapse className='collapse' collapsename="Equipements">
           <span className='collapse__text'>{currentappartment.equipments}</span> 
         </Collapse>
-        </div>
+      </div>
     </div>
   );
 }
